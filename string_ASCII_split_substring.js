37602b94f00266b5c00f79eebbefe019ad279f86
kashi159 <kashifzafar15@gmail.com>
2022-09-12 21:58:21 +0530

    Maximum Number of Words Found in Sentences

--- string_ASCII_split_substring.js
+++ string_ASCII_split_substring.js
@@ -1,4 +1,4 @@
-var x= "Kashif is a good boy"//,"I think so too","he is busy";
+var x= ["Kashif is a good boy","I think so too","he is busy"];
 // console.log(x.charCodeAt(0)); // prints ASCII Code
 // console.log(x.split(" "));  // splits the string on the selected parameters
 // console.log(x.substring(1,8)); // prints only the substring at the desired index
