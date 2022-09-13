982bb53d016931af15b6635a1653aceaa4deda87
kashi159 <kashifzafar15@gmail.com>
2022-09-13 18:38:23 +0530

    queue

---
+++ queue.js
@@ -0,0 +1,59 @@
+class Queue
+{
+    constructor()
+    {
+        this.q=[];
+        this.front=-1;
+        this.rear=-1;
+    }
+    Enqueue(ele)
+    {
+        if(this.front==-1)
+        {
+            this.front=this.front+1;
+            this.rear=this.rear+1;
+        }
+        else
+        {
+            this.rear=this.rear+1;
+        }
+        this.q[this.rear]=ele;
+    }
+    Dequeue()
+    {
+        if(this.front==this.rear)
+        {
+            this.front=-1;
+            this.rear=-1;
+        }
+        else
+        {
+            this.front=this.front+1;
+        }
+        this.q[this.front];
+    }
+    isEmpty()
+    {
+        if(this.q.length===0)
+        {
+            return true;
+        }
+    }
+    getFirstElement()
+    {
+        return this.q[this.front];
+    }
+}
+
+var obj=new Queue();
+obj.Enqueue(1);
+obj.Enqueue(2);
+obj.Enqueue(3);
+obj.Enqueue(4);
+obj.Enqueue(5);
+console.log(obj.front);
+console.log(obj.rear)
+obj.Dequeue()
+console.log(obj.front);
+console.log(obj.rear)
+console.log(obj.q)
