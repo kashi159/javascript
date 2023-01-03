c32e8f5334170e02385507ff8b3f8f034e9ad55a
kashi159 <kashifzafar15@gmail.com>
2023-01-03 17:02:33 +0530

    using multiple pointer

---
+++ uniqueValues.js
@@ -0,0 +1,19 @@
+function countUniqueValues(arr){
+    // add whatever parameters you deem necessary - good luck!
+    var i=0;
+    var j=1;
+    if(arr.length===0){
+        return 0;
+    }
+    while(j<arr.length){
+        if(arr[i]!=arr[j]){
+            i++
+            arr[i]=arr[j];
+            j++;
+        }
+        else{
+            j++;
+        }
+    }
+    return i+1;
+  }
