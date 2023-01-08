04c8606b377101473de7600518a6510d5c869b8e
kashi159 <kashifzafar15@gmail.com>
2023-01-08 15:28:25 +0530

    Naive Search

---
+++ naiveSearch.js
@@ -0,0 +1,16 @@
+function naiveSearch(long,short){
+    var count=0;
+    for(var i=0; i<long.length; i++){
+        for(var j=0; j<short.length; j++){
+            if(short[j]!==long[i+j]){
+                break;
+            }
+            if(j===short.length-1){
+                count++;
+            }
+        }
+    }
+    console.log(count);
+}
+
+naiveSearch("happy haha hag","ha")
